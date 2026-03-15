c617e54b84c8306bace9294ea63b08d0567013e1
Cristian Agudelo <crixtyam@gmail.com>
2026-03-15 12:06:34 -0500

    Implementación(repository): Creacion de repositorios (Para Cliente, Producto, Pedido y DetallePedido).

---
+++ src/main/java/com/tienda/tiendaservice/repository/PedidoRepository.java
@@ -0,0 +1,9 @@
+package com.tienda.tiendaservice.repository;
+
+import com.tienda.tiendaservice.entity.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+}
